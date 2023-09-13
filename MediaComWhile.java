282b41ccf038ac23e05600a28916da2791a4e09d
caiomar <caiomar@gmail.com>
2023-09-12 21:04:37 -0300

    ap05(repeticao): declaracao de variaveis

---
+++ MediaComWhile.java
@@ -0,0 +1,13 @@
+public class MediaComWhile{
+
+    public static void main(String[] args) {
+        //declaracão de variáveis
+        // inteiros-> byte, short, int, long
+        short numeroAlunos, contador;
+        float nota1, nota2;
+
+
+
+
+    }
+}
